675e8480b8d4dd0acb3baf8886c1e85228ff5699
kimminse3380 <mss2214@naver.com>
2022-06-19 19:57:33 +0900

    git init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game - React</h1>
+      <div className="container"></div>
+    </div>
+  );
+}
+
+export default App;
